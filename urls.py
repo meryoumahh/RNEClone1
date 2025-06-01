2b0416d84728d0aacaf2bdefd2aaa72cae3d54b5
Meryem Maatallah <111581460+meryoumahh@users.noreply.github.com>
2025-06-01 12:43:54 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('SimulateurDenomination.html', views.simulateur, name='simulateur'),
-     path('Chatbot.html', views.Chatter, name='ChatBot')
+    path('Chatbot.html', views.Chatter, name='ChatBot'),
+    path('api/chatbot/', views.chatbot_api, name='chatbot_api'),
 ]
